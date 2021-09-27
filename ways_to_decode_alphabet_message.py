60e6feb7941aa5d6abdea9eca945960fca6b6610
Jason Ventresca <jason.ventresca@oracle.com>
2021-09-27 17:06:57 -0400

    Implemented the core business logic in a new recursive function. Note this implementation still has several bugs.

--- ways_to_decode_alphabet_message.py
+++ ways_to_decode_alphabet_message.py
@@ -2,6 +2,7 @@
 
 import unittest
 import logging
+import typing
 
 """
 Daily Coding Problem / Inbox Problem #7 / Medium Difficulty
@@ -17,12 +18,67 @@ For example, "001" is not allowed.
 """
 
 
+def _get_all_valid_decodings(n: int) -> typing.Set[str]:
+    """
+        :param n:   integer from 1 to 26, representing the encoded message
+        :returns:   set of strings, representing the set of valid decodings as strings
+    """
+
+    digits = tuple([int(x) for x in str(n)])
+    ret = set()
+    print()
+    prev = None
+    past_2_digits = None
+    message = ""
+    for i, d in enumerate(digits):
+        ROOT_LOGGER.debug("evaluating: '{}'".format(d))
+        ROOT_LOGGER.debug(" -> {}".format("to do"))
+
+        try:
+            decoded_single = _decode_single_char(d)
+            message += decoded_single
+
+        except:
+            # TODO: Catch a custom (or otherwise adequately specific) exception to mitigate against a potential bug in reacting to the wrong exception.
+
+            # Assume the exception was due to attempting to decode a zero.
+            return set()
+
+        try:
+            past_2_digits = int("{}{}".format(prev, d))
+            decoded_multi = _decode_single_char(past_2_digits)
+            # The pasts two digits can be interpreted
+            # as a valid, single character.
+            # Branch off to a new message that will be
+            # merged into our final result set.
+            ret.add(
+                message[:-2] \
+                + decoded_multi \
+                + (
+                    _get_all_valid_decodings(digits[i:])
+                    if i < len(digits) - 1 else ""
+                )
+            )
+
+        except: pass
+
+        prev = d
+
+    ret.add(message)
+
+    return ret
+
+
 def decode_message(n: int) -> int:
-    return 0
+    """
+        :param n:   integer from 1 to 26 representing the encoded message
+        :returns:   positive integer, the total number of valid messages that can be decoded from the input message
+    """
+    return len(_get_all_valid_decodings(n))
 
 
 def _decode_single_char(n: int) -> str:
-    error_message = "n must be an integer from 1 to 26"
+    error_message = "n must be an integer from 1 to 26: n had value {}, type {}".format(n, type(n))
     assert type(n) == int, error_message
     assert 1 <= n <= 26, error_message
 
