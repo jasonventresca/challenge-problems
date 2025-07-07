e54d17eac1b05a869867f808ae0735fcf80628f9
Jason Ventresca <jasonventresca@gmail.com>
2025-07-07 13:00:31 -0700

    easy solution; kind of cheating, using python's built-ins.

---
+++ remove-duplicates-from-sorted-array.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+DEBUG = True
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if DEBUG: print(f'### input: {nums}')
+
+        dedup = list(sorted(set(nums)))
+        for i, v in enumerate(dedup):
+            nums[i] = dedup[i]
+        k = len(dedup)
+        return k
+
+def main():
+    test_cases = [
+        # Test case #1
+        (
+            # Input
+            [1,1,2],
+            # Expected Output
+            [1,2],
+        ),
+    ]
+    s = Solution()
+    for (input_, expected_output) in test_cases:
+        k = s.removeDuplicates(input_)
+        assert k == len(expected_output)
+        assert (expected_output == input_[:k]), \
+            f'\nexpected: {expected_output}' \
+            f'\n  result: {input_}'
+    print('All tests passed : )')
+
+if __name__ == '__main__':
+    main()
