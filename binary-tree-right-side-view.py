5035d72cabaa9af46b52f51020f6f5c3c4e6af42
Jason Ventresca <jasonventresca@gmail.com>
2025-09-29 14:19:57 -0700

    Set up a new problem.

---
+++ binary-tree-right-side-view.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/binary-tree-right-side-view
+#   difficulty: Medium
+#   topics: Tree, Depth-First Search, Breadth-First Search, Binary Tree
+''' Run with the following commands (replace file name):
+    $ pipenv shell
+    $ py.test --log-cli-level=debug -v --tb=no binary-tree-right-side-view.py
+'''
+
+from typing import List, Optional
+import logging
+from collections import defaultdict
+import math
+
+import pytest
+from icecream import ic
+
+logger = logging.getLogger(__name__)
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return ', '.join([
+            str(self.val),
+            self.left.__str__(),
+            self.right.__str__(),
+        ])
+
+    def __str__(self):
+        return self.__repr__()
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        print(f'root: {root}')
+
+@pytest.mark.parametrize(
+    'input_data, expected_output',
+    [
+        # Test case #1
+        (
+            # Input
+            TreeNode(
+                val=1,
+                left=TreeNode(
+                    val=2,
+                    left=None,
+                    right=TreeNode(val=5)
+                ),
+                right=TreeNode(
+                    val=3,
+                    left=None,
+                    right=TreeNode(val=4)
+                ),
+            ),
+            # Expected Output
+            [1,3,4],
+        ),
+        # Test case #2
+        (
+            # Input
+            TreeNode(
+                val=1,
+                left=TreeNode(
+                    val=2,
+                    left=TreeNode(
+                        val=4,
+                        left=TreeNode(val=5),
+                        right=None,
+                    ),
+                    right=None,
+                ),
+                right=TreeNode(
+                    val=3,
+                    left=None,
+                    right=None,
+                ),
+            ),
+            # Expected Output
+            [1,3,4,5],
+        ),
+        # Test case #3
+        (
+            # Input
+            TreeNode(
+                val=1,
+                left=None,
+                right=TreeNode(
+                    val=3,
+                    left=None,
+                    right=None,
+                ),
+            ),
+            # Expected Output
+            [1,3],
+        ),
+        # Test case #4
+        (
+            # Input
+            TreeNode(),
+            # Expected Output
+            [],
+        ),
+    ],
+)
+def test_case(input_data, expected_output):
+    s = Solution()
+    result = s.rightSideView(input_data)
+    assert expected_output == result
