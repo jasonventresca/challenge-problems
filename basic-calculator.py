2c529ed7700d67ed32cef8a381d3ce0a3cccc607
Jason Ventresca <jasonventresca@gmail.com>
2025-10-01 16:29:13 -0400

    Making some progress. Implemented basic addition, including chaining multiple addition operations. No support for minus, parens, etc. yet.

--- basic-calculator.py
+++ basic-calculator.py
@@ -20,7 +20,37 @@ logger = logging.getLogger(__name__)
 class Solution:
     def calculate(self, s: str) -> int:
         logger.debug(f's: {s}')
-        return 2
+        logger.debug('-' * 30)
+        digits = tuple('1234567890')
+        rhs = None
+        operator = None
+        result = 0
+        for c in s:
+            logger.debug(f'c: {c}')
+            if c in digits:
+                logger.debug('-> it\'s a digit')
+                if operator is None:
+                    result += int(c)
+                elif operator == '+':
+                    rhs = int(c)
+                    result += rhs
+                    operator, rhs = None, None
+                else:
+                    raise ValueError('Unexpected operator: "{operator}" !')
+            elif c == '(':
+                raise NotImplementedError('(')
+            elif c == ')':
+                raise NotImplementedError(')')
+            elif c == '+':
+                operator = '+'
+            elif c == '-':
+                raise NotImplementedError('-')
+            elif c == ' ':
+                pass
+            else:
+                raise ValueError('Parser got unexpected token: "{c}" !')
+
+        return result
 
 @pytest.mark.parametrize(
     'input_data, expected_output',
@@ -32,20 +62,26 @@ class Solution:
             # Expected Output
             2,
         ),
-        # Test case #2
         (
             # Input
-            ' 2-1 + 2 ',
+            '1 + 1 + 2',
             # Expected Output
-            3,
+            4,
         ),
-        # Test case #3
-        (
-            # Input
-            '(1+(4+5+2)-3)+(6+8)',
-            # Expected Output
-            23,
-        ),
+        ## Test case #2
+        #(
+        #    # Input
+        #    ' 2-1 + 2 ',
+        #    # Expected Output
+        #    3,
+        #),
+        ## Test case #3
+        #(
+        #    # Input
+        #    '(1+(4+5+2)-3)+(6+8)',
+        #    # Expected Output
+        #    23,
+        #),
     ],
 )
 def test_case(input_data, expected_output):
