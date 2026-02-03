362e1b620c92be304a448883e71e92dbea744eda
Jason Ventresca <jasonventresca@gmail.com>
2026-02-03 09:50:28 -0600

    Implemented as one-indexed, which is a bit more intuitive.

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -30,8 +30,22 @@ Solution
 '''
 class Solution:
     def climbStairs(self, n: int) -> int:
+        return self.dp1(n)
+
+    def dp1(self, n: int) -> int:
         logger.debug(f'input: {n}')
         dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+
+    def dp0(self, n: int) -> int:
+        logger.debug(f'input: {n}')
+        dp = [0] * n
         dp[0] = 1
         dp[1] = 2
         for i in range(2, n):
